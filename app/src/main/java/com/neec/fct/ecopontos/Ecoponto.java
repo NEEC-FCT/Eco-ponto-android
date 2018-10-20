1d9b6e21547f4493ff1679aefc691bcc1c4817a9
veloso14 <jm.veloso@campus.fct.unl.pt>
2018-10-20 19:32:59 +0100

    Classe Ecoponto

---
+++ app/src/main/java/com/neec/fct/ecopontos/Ecoponto.java
@@ -0,0 +1,41 @@
+package com.neec.fct.ecopontos;
+
+import com.tomtom.online.sdk.common.location.LatLng;
+
+public class Ecoponto {
+
+    private String descricao;
+    private  boolean verde;
+    private boolean amarelo;
+    private  boolean azul;
+    private LatLng location;
+
+    public  Ecoponto(String descricao , boolean verde , boolean amarelo , boolean azul , LatLng location){
+        this.descricao= descricao;
+        this.verde = verde;
+        this.amarelo = amarelo;
+        this.azul = azul;
+        this.location = location;
+
+    }
+
+    public boolean isAmarelo() {
+        return amarelo;
+    }
+
+    public boolean isVerde() {
+        return verde;
+    }
+
+    public boolean isAzul() {
+        return azul;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+}
