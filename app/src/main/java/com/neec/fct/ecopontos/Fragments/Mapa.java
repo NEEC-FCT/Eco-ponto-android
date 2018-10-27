0da18b4f539c872f99d5bfbe154b8b7772ebdf44
veloso14 <jm.veloso@campus.fct.unl.pt>
2018-10-27 17:56:16 +0100

    Calcular distancias com latlong

--- app/src/main/java/com/neec/fct/ecopontos/Fragments/Mapa.java
+++ app/src/main/java/com/neec/fct/ecopontos/Fragments/Mapa.java
@@ -213,4 +213,36 @@ public class Mapa extends Fragment implements FunctionalExampleFragment {
     public void disableOptionsView() {
 
     }
+
+
+
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * @returns Distance in Meters
+     */
+    public static double distance(LatLng inicio , LatLng fim ) {
+
+        double lat1 = inicio.getLatitude();
+        double lat2 = fim.getLatitude();
+        double lon1 = inicio.getLongitude();
+        double lon2 = fim.getLongitude();
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        distance = Math.pow(distance, 2);
+
+        return Math.sqrt(distance);
+    }
 }
