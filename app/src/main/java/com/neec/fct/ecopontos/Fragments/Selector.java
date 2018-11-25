2d4444aad775c87d54f983b61f9fa69a53a8252c
veloso14 <jm.veloso@campus.fct.unl.pt>
2018-11-25 01:38:38 +0000

    Selector Option

--- app/src/main/java/com/neec/fct/ecopontos/Fragments/Selector.java
+++ app/src/main/java/com/neec/fct/ecopontos/Fragments/Selector.java
@@ -1,9 +1,14 @@
 package com.neec.fct.ecopontos.Fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -60,18 +65,76 @@ public class Selector extends Fragment implements RecyclerViewAdapter.ItemListen
         recyclerView.setLayoutManager(layoutManager);
 
 
-        /**
-         Simple GridLayoutManager that spans two columns
-         **/
-        /*GridLayoutManager manager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(manager);*/
         return  view;
     }
 
     @Override
     public void onItemClick(DataModel item) {
 
-        System.out.println(item.toString());
+        System.out.println("item: " + item.text.toString());
+
+        if( item.text.toString().equals("Papel ou Cartão")){
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new PaperChoosed())
+                    .commit();
+        }
+
+        //Residuo perigoso
+        //done
+        else if( item.text.toString().equals("Resíduo Perigoso")){
+            String url = "https://bd2.fct.unl.pt/v7/downloads/dat/ef025_03_entrega_res_perigosos_0118.pdf";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+        else if( item.text.toString().equals("Óleo lubrificante")){
+            //i.	Deposite num recipiente estanque e fechado
+            String url = "https://bd2.fct.unl.pt/v7/downloads/dat/ef025_03_entrega_res_perigosos_0118.pdf";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+
+        else if( item.text.toString().equals("Mobiliário")){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setTitle("Indicações");
+            builder.setMessage("Solicite à Direção o abate do equipamento\n" +
+                    "Após autorização para o abate solicite a recolha à DAT (pedido de assistência), através do email\n" +
+                    "div.at.sg.helpdesk@fct.unl.pt");
+
+            builder.setPositiveButton("Já tenho", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int which) {
+                    // Do nothing but close the dialog
+
+
+                    dialog.dismiss();
+
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                            "mailto","div.at.sg.helpdesk@fct.unl.pt", null));
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mobiliário abate");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Corpo");
+                    startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                }
+            });
+
+            builder.setNegativeButton("ok", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    // Do nothing
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
 
     }
 
