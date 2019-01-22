82a707fbe5b2e83040be7c6adeebfba52aaa5f3f
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-01-22 00:34:24 +0000

    Loading

--- app/src/main/java/com/neec/fct/ecopontos/Loading.java
+++ app/src/main/java/com/neec/fct/ecopontos/Loading.java
@@ -23,7 +23,7 @@ public class Loading extends AppCompatActivity {
         new Handler().postDelayed(new  Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(Loading.this,MainActivity.class);
+                Intent homeIntent = new Intent(Loading.this,LoginActivity.class);
                 startActivity(homeIntent);
                 this.finish();
             }
